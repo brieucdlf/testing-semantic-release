2c68c48232eb62bec0deba7455f7d3057d38a4a7
brieucdlf <contact@brieucdlf.fr>
2025-03-13 22:21:14 +0100

    feat: update title
    BREAKING CHANGE: title changed from "testing-semanting-release v1" to "testing-semanting-release v1 breaking
     change"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from "@angular/router";
   styleUrl: "./app.component.scss",
 })
 export class AppComponent {
-  title = "testing-semanting-release v1";
+  title = "testing-semanting-release v1 breaking change";
 }
